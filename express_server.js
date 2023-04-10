69167664de470ca15a603016d03e986f40db4aef
MohibHome <mohib@can-health.org>
2023-04-10 11:55:18 -0700

    pushing all the changes before start adding EJS template

--- express_server.js
+++ express_server.js
@@ -21,4 +21,13 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>world</b></body></html>\n');
-})
+});
+
+app.get('/set', (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+});
+
+app.get('/fetch', (req, res) => {
+    res.send(`a = ${a}`);
+});
