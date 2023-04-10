479c925ebff49ab4c6dcb6fb791c652698b1f33b
MohibHome <mohib@can-health.org>
2023-04-10 14:57:09 -0700

    updated express_server file

--- express_server.js
+++ express_server.js
@@ -5,6 +5,11 @@ const PORT = 8080;
 //This tells the Express app to use EJS as its templating engine
 app.set('view engine', 'ejs');
 
+// This is middleware from Express, which will translate, or parse the body.
+/* The body-parser library will convert the request body from .. 
+    .. a Buffer into string that we can read.  */
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -16,10 +21,20 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
+
+app.get('/urls/new', (req, res) => {
+    res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render('urls_show', templateVars);
 });
+
 
 app.get('/', (req, res) => {
     res.send("Hello!");
